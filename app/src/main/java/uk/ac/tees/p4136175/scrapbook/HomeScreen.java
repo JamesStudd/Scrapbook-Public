eb3388cb53261d14b45dd45e7dc4620052de8f34
James Studd <james.studd@live.co.uk>
2017-03-07 14:55:13 +0000

    Added AdventureEntry class

--- app/src/main/java/uk/ac/tees/p4136175/scrapbook/HomeScreen.java
+++ app/src/main/java/uk/ac/tees/p4136175/scrapbook/HomeScreen.java
@@ -2,13 +2,19 @@ package uk.ac.tees.p4136175.scrapbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity implements android.view.View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         // asdasdad
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
