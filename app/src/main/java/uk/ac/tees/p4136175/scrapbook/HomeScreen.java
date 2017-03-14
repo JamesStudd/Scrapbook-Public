0bd1cc5cc655250fd7a49d0b7b012b1682d16440
p4136175 <james.studd@live.co.uk>
2017-03-14 16:27:07 +0000

    Copied stuff

--- app/src/main/java/uk/ac/tees/p4136175/scrapbook/HomeScreen.java
+++ app/src/main/java/uk/ac/tees/p4136175/scrapbook/HomeScreen.java
@@ -1,20 +1,38 @@
 package uk.ac.tees.p4136175.scrapbook;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity implements android.view.View.OnClickListener{
+
+    Button btnAdd, btnList;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        // asdasdad
+
+        btnAdd = (Button) findViewById(R.id.addButton);
+        btnAdd.setOnClickListener(this);
+
+        btnList = (Button) findViewById(R.id.listButton);
+        btnList.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-
+        if (v == findViewById(R.id.addButton)) {
+            Intent intent = new Intent(context, MakeAdventure.class);
+            startActivity(intent);
+        } else if (v == findViewById(R.id.listButton)){
+            Intent intent = new Intent(context, AdventureList.class);
+            startActivity(intent);
+        }
     }
 }
